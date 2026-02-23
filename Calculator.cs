dcdffd218a41491546002d6b98cb1a019023d330
Munuhd4 <m.nurul@students.amikom.ac.id>
2026-02-23 21:58:25 +0700

    Operator

--- Calculator.cs
+++ Calculator.cs
@@ -3,10 +3,11 @@ class calculator
 	static void Main(string[] args)
 	{
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}",a, b, Penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}",a, b, Pengurangan(a,b));
 		
-		
-		
-		
+		Console.WriteLine("\nTekan Sembarang untuk keluar");
+		Console,ReadKey();
 		
 	
 
