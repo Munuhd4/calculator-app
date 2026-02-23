348340dfe887f46d509125568d797854f048cd2c
Munuhd4 <m.nurul@students.amikom.ac.id>
2026-02-23 21:37:17 +0700

    Tambah Sedikit

--- Calculator.cs
+++ Calculator.cs
@@ -3,5 +3,29 @@ class calculator
 	static void Main(string[] args)
 	{
 		
+		
+		
+		
+		
+		
+	
+
+
+	
 	}
+	
+	
+	static int Penambahan(int a, intb)
+	{
+		return a + b;
+	}
+	
+	
+	static int Pengurangan(int a, intb)
+	{
+		return a - b;
+	}
+	
+	
+	
 }
